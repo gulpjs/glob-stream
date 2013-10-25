ba15a2e139333e3dd8ff99e410d9c0eb0403b3f1
Contra <contra@maricopa.edu>
2013-10-24 20:36:24 -0700

    js rewrite in js. also closes #1 and closes #2

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
-require('coffee-script');
-module.exports = require('./lib/main');
+var es = require('event-stream');
+var glob = require('glob');
+var path = require('path');
+
+module.exports = {
+  create: function(globString, opt) {
+    if (typeof globString !== 'string') {
+      throw new Error("Invalid or missing glob string");
+    }
+
+    if (!opt) opt = {};
+    if (typeof opt.silent !== 'boolean') opt.silent = true;
+    if (typeof opt.nonull !== 'boolean') opt.nonull = false;
+
+    
+    var stream = es.pause();
+    var globber = new glob.Glob(globString, opt);
+    var rules = globber.minimatch.set[0];
+    var basePath = path.normalize(rules.map(function(s, idx) {
+      if (typeof s === 'string' && idx !== rules.length-1) {
+        return s;
+      } else {
+        return '';
+      }
+    }).join(path.sep));
+
+    globber.on('error', stream.emit.bind(stream, 'error'));
+    globber.on('end', function(){
+      stream.end();
+    });
+    globber.on('match', function(filename) {
+      stream.write({
+        base: basePath,
+        path: filename
+      });
+    });
+
+    return stream;
+  }
+};
