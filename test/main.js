5ebe683f07d05f56b43b76cbfa84dbfa4eff15ae
Blaine Bublitz <blaine.bublitz@gmail.com>
2016-09-03 10:53:33 -0700

    Fix: Upgrade glob-parent to fix incorrect base when parens in glob path (closes #70)

--- test/main.js
+++ test/main.js
@@ -80,6 +80,24 @@ describe('glob-stream', function() {
       });
     });
 
+    it('should set the correct base when ( ) in glob', function(done) {
+      var stream = gs.create('./fixtures/has (parens)/*.dmc', { cwd: __dirname });
+      should.exist(stream);
+      stream.on('error', function(err) {
+        throw err;
+      });
+      stream.on('data', function(file) {
+        should.exist(file);
+        should.exist(file.path);
+        should.exist(file.base);
+        should.exist(file.cwd);
+        String(file.cwd).should.equal(__dirname);
+        String(file.base).should.equal(join(__dirname, 'fixtures/has (parens)/'));
+        String(join(file.path,'')).should.equal(join(__dirname, './fixtures/has (parens)/test.dmc'));
+        done();
+      });
+    });
+
     it('should find files in paths that contain ( )', function(done) {
       var stream = gs.create('./fixtures/**/*.dmc', { cwd: __dirname });
       var files = [];
