bfbd180040235e2ae960c96a17b4eb25764fc3a0
Blaine Bublitz <blaine.bublitz@gmail.com>
2017-02-21 16:04:38 -0700

    Fix: Revert breaking changes in 4.1.0 release

--- index.js
+++ index.js
@@ -26,19 +26,11 @@ var gs = {
     // create stream and map events from globber to it
     var stream = through2.obj(negatives.length ? filterNegatives : undefined);
 
-    var found = false;
-
     globber.on('error', stream.emit.bind(stream, 'error'));
-    globber.on('end', function(){
-      if (opt.allowEmpty !== true && !found && globIsSingular(globber)) {
-        stream.emit('error', new Error('File not found with singular glob'));
-      }
-
+    globber.on('end', function(/* some args here so can't use bind directly */){
       stream.end();
     });
     globber.on('match', function(filename) {
-      found = true;
-
       stream.write({
         cwd: opt.cwd,
         base: basePath,
@@ -103,13 +95,8 @@ var gs = {
     // then just pipe them to a single unique stream and return it
     var aggregate = new Combine(streams);
     var uniqueStream = unique('path');
-    var returnStream = aggregate.pipe(uniqueStream);
 
-    aggregate.on('error', function (err) {
-      returnStream.emit('error', err);
-    });
-
-    return returnStream;
+    return aggregate.pipe(uniqueStream);
 
     function streamFromPositive(positive) {
       var negativeGlobs = negatives.filter(indexGreaterThan(positive.index)).map(toGlob);
@@ -145,18 +132,6 @@ function indexGreaterThan(index) {
 
 function toGlob(obj) {
   return obj.glob;
-}
-
-function globIsSingular(glob) {
-  var globSet = glob.minimatch.set;
-
-  if (globSet.length !== 1) {
-    return false;
-  }
-
-  return globSet[0].every(function isString(value) {
-    return typeof value === 'string';
-  });
 }
 
 module.exports = gs;
